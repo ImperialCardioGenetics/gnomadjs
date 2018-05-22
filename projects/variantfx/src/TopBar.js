31485f66353a93decd12bffa708658fadeb12633
Mark Woodbridge <m.woodbridge@imperial.ac.uk>
2018-05-22 06:29:02 +0100

    Fix gene search bar on homepage

--- projects/variantfx/src/TopBar.js
+++ projects/variantfx/src/TopBar.js
@@ -3,7 +3,9 @@ import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
-import { currentGene, actions as activeActions } from '@broad/redux-genes'
+import { withRouter } from 'react-router-dom'
+
+import { currentGene, actions as geneActions } from '@broad/redux-genes'
 
 import {
   DataSelectionContainer,
@@ -15,18 +17,33 @@ import {
   actions as variantFxActions,
 } from './redux'
 
+const GENES = [
+  "ACTC1",
+  "CSRP3",
+  "LMNA",
+  "MYBPC3",
+  "MYH7",
+  "MYL2",
+  "MYL3",
+  "PLN",
+  "TNNC1",
+  "TNNI3",
+  "TNNT2",
+  "TPM1",
+  "TTN"
+]
+
 const TopBarContainer = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
-  align-items: center;
   width: 100%;
   height: 40px;
-  padding-top: 10px;
+  padding-top: 20px;
   margin-bottom: 20px;
   border-bottom: 1px solid #000;
   background-color: #B71C1C;
-  box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
+  ${'' /* box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23); */}
 `
 
 const Logo = styled.div`
@@ -42,10 +59,18 @@ const Search = styled.div`
   font-size: 15px;
 `
 
+const SearchIconContainer = styled.span`
+  position: absolute;
+  left: 7px;
+  font-size: 15px;
+`
+
 const SearchInput = styled.input`
   height: 20px;
   width: 275px;
   background-color: white;
+  /*margin-top: 2px;*/
+  /*margin-right: 100px;*/
   text-indent: 30px;
   -webkit-transition: width 0.4s ease-in-out;
   transition: width 0.4s ease-in-out;
@@ -66,10 +91,11 @@ const MenuItem = styled.div`
 
 const StyledLink = styled(Link)`
   text-decoration: none;
-  color: white;
 `
 
 const TopBar = ({
+  history,
+  currentGene,
   setCurrentGene,
   uniqueGeneDiseaseNames,
   currentDisease,
@@ -79,7 +105,7 @@ const TopBar = ({
     <TopBarContainer>
       <StyledLink to={'/'}>
         <Logo>
-        VariantFX
+          VariantFX
         </Logo>
       </StyledLink>
       <DataSelectionContainer>
@@ -92,33 +118,33 @@ const TopBar = ({
         </select>
       </DataSelectionContainer>
       <Search>
-        {/* <SearchIconContainer>
-        </SearchIconContainer> */}
         <form>
           <SearchInput
             onChange={(event) => {
-              if (uniqueGeneDiseaseNames.contains(event.target.value)) {
-                event.target.blur();
-                setCurrentGene(event.target.value);
+              const geneName = event.target.value.toUpperCase()
+              if (GENES.includes(geneName)) {
+                event.target.blur()
+                if (currentGene) {
+                  setCurrentGene(geneName)
+                }
+                history.push(`/gene/${geneName}`)
               }
             }}
             type="text"
             name="search"
-            placeholder="Search by gene, transcript, region, or variant"
+            placeholder="Search gene"
             list="genes"
           />
           <datalist id="genes">
-            {uniqueGeneDiseaseNames.map(gene => (
-              <option key={`${gene}`} value={gene} />
-            ))}
+            {GENES.map(gene => <option value={gene} key={gene}/>)}
           </datalist>
         </form>
       </Search>
       <Menu>
-        <MenuItem><StyledLink to={'/about'}> About </StyledLink></MenuItem>
-        <MenuItem><StyledLink to={'/terms'}> Terms </StyledLink></MenuItem>
-        <MenuItem><StyledLink to={'/contact'}> Contact </StyledLink></MenuItem>
-        <MenuItem><StyledLink to={'/faq'}> FAQ </StyledLink></MenuItem>
+        <StyledLink to={'/about'}><MenuItem>About</MenuItem></StyledLink>
+        <StyledLink to={'/terms'}><MenuItem>Terms</MenuItem></StyledLink>
+        <StyledLink to={'/contact'}><MenuItem>Contact</MenuItem></StyledLink>
+        <StyledLink to={'/faq'}><MenuItem>FAQ</MenuItem></StyledLink>
       </Menu>
     </TopBarContainer>
   )
@@ -133,7 +159,7 @@ const mapStateToProps = (state) => {
 }
 const mapDispatchToProps = (dispatch) => {
   return {
-    setCurrentGene: geneName => dispatch(activeActions.setCurrentGene(geneName)),
+    setCurrentGene: geneName => dispatch(geneActions.setCurrentGene(geneName)),
     setCurrentDisease: disease => dispatch(variantFxActions.setCurrentDisease(disease)),
   }
 }
@@ -145,4 +171,4 @@ TopBar.propTypes = {
   currentDisease: PropTypes.string.isRequired,
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(TopBar)
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(TopBar))
